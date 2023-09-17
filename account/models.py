331beaf6b63e454f73540005060b46698fbeb6f2
adamorujov <ademorujov@gmail.com>
2023-09-18 03:09:52 +0400

    Problem fixed

--- account/models.py
+++ account/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib import auth
 from django.contrib.auth.models import AbstractUser, BaseUserManager
-from service.models import AccountantModel
 
 class UserManager(BaseUserManager):
     use_in_migrations = True
